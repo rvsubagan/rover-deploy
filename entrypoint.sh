a9abc63b649f1de8ee7cacbf763c02a391701715
inamicode <inamicode@gmail.com>
2022-11-22 18:50:09 +0800

    fix x 4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-if [ $INPUT_IMAGE != x ]
-then
-  caprover deploy -h $INPUT_HOST -p $INPUT_PASSWORD -a $INPUT_APP -i $INPUT_IMAGE
-else
-  caprover deploy -h $INPUT_HOST -p $INPUT_PASSWORD -a $INPUT_APP -i $INPUT_IMAGE
-fi
+caprover deploy -h ${{inputs.host}} -p ${{inputs.password}} -a ${{inputs.app}} -i ${{inputs.image}}
