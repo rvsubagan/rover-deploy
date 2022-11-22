d260b82db09f3ff4a37b10c276f7ec86e166b6de
inamicode <inamicode@gmail.com>
2022-11-22 18:04:50 +0800

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16
+FROM node:14
 
 RUN yarn global add caprover --prefix /usr/local
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,1 @@
-#!/bin/sh
 caprover deploy -h $INPUT_HOST -p $INPUT_PASSWORD -a $INPUT_APP -i $INPUT_IMAGE
