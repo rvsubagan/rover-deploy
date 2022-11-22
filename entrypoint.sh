5e96b86e3752e5bcff3ce0fe2cdba04fa9339c34
inamicode <inamicode@gmail.com>
2022-11-22 18:24:40 +0800

    fix fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,8 @@
-caprover deploy -h $INPUT_HOST -p $INPUT_PASSWORD -a $INPUT_APP -i $INPUT_IMAGE
+#!/bin/sh
+
+if [ x$INPUT_IMAGE != x ]
+then
+  caprover deploy -h $INPUT_HOST -p $INPUT_PASSWORD -a $INPUT_APP -i $INPUT_IMAGE
+else
+  caprover deploy -h $INPUT_HOST -p $INPUT_PASSWORD -a $INPUT_APP -b $INPUT_BRANCH
+fi
